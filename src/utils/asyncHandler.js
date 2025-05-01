502c8a157acb8e376eeb4016d77afa8555e2be5a
prabuddhaganvir <prabuddhapg12@gmail.com>
2025-05-01 13:19:28 +0530

    add utils files

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,19 @@
+
+export { asyncHandler }
+
+
+const asyncHandler = (fn) => async () => {
+    try {
+        await fn(req , res, next)
+
+
+
+
+    } catch (error) {
+        res.status(error.code || 500).json({
+            success:false,
+            message:error.message
+        })
+    }
+
+}
