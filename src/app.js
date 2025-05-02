fd4536798e08681e8d678e48a883c453dafab740
prabuddhaganvir <prabuddhapg12@gmail.com>
2025-05-02 09:19:48 +0530

    all setup  configure

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import cors from "cors"
 import cookieParser from 'cookie-parser'
 
 
-
 const app = express()
 
 app.use(cors({
@@ -16,10 +15,13 @@ app.use(express.urlencoded({extended:true, limit:"15kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+// routes 
 
+import userRouter from './routes/user.routes.js'
 
+//routes declaration
 
-
+app.use("/api/v1/users",userRouter)
 
 
 
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,19 +1,9 @@
 
-export { asyncHandler }
-
-
-const asyncHandler = (fn) => async () => {
-    try {
-        await fn(req , res, next)
-
-
-
-
-    } catch (error) {
-        res.status(error.code || 500).json({
-            success:false,
-            message:error.message
-        })
-    }
+const asyncHandler = (requestHandler) =>{ 
+    return (req,res,next) => {
+    Promise.resolve(requestHandler(req,res,next)).catch((err) =>(err))
+    
+}
 
 }
+export { asyncHandler }
